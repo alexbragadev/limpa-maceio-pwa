627cbcab18d0f8f69676433af97f7432caaba34d
AlexAraujoDeBraga <alexbraga107@gmail.com>
2024-03-18 16:47:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'limpa-maceio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
